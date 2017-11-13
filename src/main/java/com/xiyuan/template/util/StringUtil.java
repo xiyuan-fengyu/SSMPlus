d590e86249f4d33efbaa8f11dde9b9efe2455414
xiyuan_fengyu <xiyuan_fengyu@163.com>
2017-11-13 15:19:39 +0800

    添加一些工具类

---
+++ src/main/java/com/xiyuan/template/util/StringUtil.java
@@ -0,0 +1,107 @@
+package com.xiyuan.template.util;
+
+/**
+ * Created by xiyuan_fengyu on 2017/8/17.
+ */
+public class StringUtil {
+
+    public static int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0; i< m+1; i++){
+            dp[0][i] = i;
+        }
+        for(int i=0; i<n+1; i++){
+            dp[i][0] = i;
+        }
+
+
+        for(int i = 1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static int longestCommonSubstring(String word1, String word2) {
+        // state: f[i][j] is the length of the longest lcs
+        // ended with A[i - 1] & B[j - 1] in A[0..i-1] & B[0..j-1]
+        int n = word1.length();
+        int m = word2.length();
+        int[][] f = new int[n + 1][m + 1];
+
+        // initialize: f[i][j] is 0 by default
+
+        // function: f[i][j] = f[i - 1][j - 1] + 1 or 0
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                } else {
+                    f[i][j] = 0;
+                }
+            }
+        }
+
+        // answer: max{f[i][j]}
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                max = Math.max(max, f[i][j]);
+            }
+        }
+
+        return max;
+    }
+
+    public int longestCommonSubsequence(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int f[][] = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+                if(str1.charAt(i - 1) == str2.charAt(j - 1))
+                    f[i][j] = f[i - 1][j - 1] + 1;
+            }
+        }
+        return f[n][m];
+    }
+
+
+    private static final char[] chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    public static String randomStr(int len) {
+        if (len <= 0) return "";
+
+        char[] randChars = new char[len];
+        for (int i = 0; i < len; i++) {
+            randChars[i] = chars[(int) (Math.random() * chars.length)];
+        }
+        return String.valueOf(randChars);
+    }
+
+    public static String randomStrWithTime(int len, String divider) {
+        StringBuilder builder = new StringBuilder();
+        int charsLen = chars.length;
+        for (int i = 0; i < len; i++) {
+            builder.append(chars[(int) (Math.random() * charsLen)]);
+        }
+        builder.append(divider);
+
+        long now = System.currentTimeMillis();
+        while (now > 0) {
+            builder.append(chars[(int) (now % charsLen)]);
+            now /= charsLen;
+        }
+        return builder.toString();
+    }
+
+}
---
+++ src/main/java/com/xiyuan/template/util/Util.java
@@ -0,0 +1,87 @@
+package com.xiyuan.template.util;
+
+import com.google.gson.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+public class Util {
+
+    public static byte[] readAllBytesFromResource(String resourcePath) {
+        try (InputStream in = Util.class.getClassLoader().getResourceAsStream(resourcePath)) {
+            byte[] bytes = new byte[in.available()];
+            in.read(bytes);
+            return bytes;
+        }
+        catch (Exception e) {
+//            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String readFromResource(String resourcePath, Charset charset) {
+        byte[] bytes = readAllBytesFromResource(resourcePath);
+        return bytes == null ? null : new String(bytes, charset);
+    }
+
+    public static byte[] readAllBytesFromFile(String filePath) {
+        try (InputStream in = new FileInputStream(filePath)) {
+            byte[] bytes = new byte[in.available()];
+            in.read(bytes);
+            return bytes;
+        }
+        catch (Exception e) {
+//            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String readFromFile(String filePath, Charset charset) {
+        byte[] bytes = readAllBytesFromFile(filePath);
+        return bytes == null ? null : new String(bytes, charset);
+    }
+
+    private static final JsonParser jsonParser = new JsonParser();
+
+    public static final Gson gson = new Gson();
+
+    public static final Gson gsonFormat = new GsonBuilder().setPrettyPrinting().create();
+
+    public static JsonElement toJsonElement(String json) {
+        return jsonParser.parse(json);
+    }
+
+    public static JsonObject toJsonObject(String json) {
+        return jsonParser.parse(json).getAsJsonObject();
+    }
+
+    public static JsonArray toJsonArray(String json) {
+        return jsonParser.parse(json).getAsJsonArray();
+    }
+
+    public static JsonElement readJsonFromResource(String resourcePath, Charset charset) {
+        return toJsonElement(readFromResource(resourcePath, charset));
+    }
+
+    public static JsonElement readJsonFromFile(String filePath, Charset charset) {
+        return toJsonElement(readFromFile(filePath, charset));
+    }
+
+    public static boolean writeToFile(String filePath, String content, Charset charsets) {
+        File file = new File(filePath);
+        if (file.getParentFile().exists() || file.getParentFile().mkdirs()) {
+            try (FileOutputStream out = new FileOutputStream(file)) {
+                out.write(content.getBytes(charsets));
+                return true;
+            }
+            catch (Exception e) {
+//            e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+}
