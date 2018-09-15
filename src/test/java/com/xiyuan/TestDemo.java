2c4357b7d38cc57fbb7adc717197913972600d2a
xiyuan_fengyu <xiyuan_fengyu@163.com>
2018-09-15 10:23:41 +0800

    增加测试 spring-test5 junit5

--- src/test/java/com/xiyuan/TestDemo.java
+++ src/test/java/com/xiyuan/TestDemo.java
@@ -1,16 +1,11 @@
 package com.xiyuan;
 
-import com.xiyuan.template.util.ResponseMap;
-import com.xiyuan.template.util.Util;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import redis.clients.jedis.Jedis;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by xiyuan_fengyu on 2018/9/15 10:19.
